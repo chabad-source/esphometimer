af2464bbedcad17b8bcce4d0d0f5eeddf73cad54
Samuel Sieb <samuel@sieb.net>
2024-05-11 22:30:11 -0700

    typed outputs

--- components/timer/__init__.py
+++ components/timer/__init__.py
@@ -1,10 +1,12 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome import automation
 from esphome.components import button, select, switch, text
 from esphome.const import (
     CONF_ID,
     CONF_MODE,
     CONF_OUTPUTS,
+    CONF_THEN,
 )
 
 """
@@ -19,8 +21,11 @@ timer:
   disable_switch:
     name: Disable Timers
   outputs:
-    - relay_1
-    - relay_2
+    - type: switch
+      id: relay_1
+    - type: automation
+      then:
+        - stuff
   #optional
   names:
     - "Timer 1"
@@ -40,6 +45,8 @@ CONF_TIMER_SELECT = "timer_select"
 CONF_SAVE_BUTTON = "save_button"
 CONF_DISABLE_SWITCH = "disable_switch"
 CONF_NAMES = "names"
+CONF_SWITCH = "switch"
+CONF_AUTOMATION = "automation"
 
 def validate_names(config):
     if CONF_NAMES not in config:
@@ -47,6 +54,17 @@ def validate_names(config):
     if len(config[CONF_NAMES]) != config[CONF_QUANTITY]:
         raise cv.Invalid("number of names must be the same as the timer quantity")
     return config
+
+OUTPUT_SCHEMA = cv.typed_schema(
+    {
+        CONF_SWITCH: {
+            cv.GenerateID(CONF_ID): cv.use_id(switch.Switch),
+        },
+        CONF_AUTOMATION: {
+            cv.Required(CONF_THEN): automation.validate_automation(single=True),
+        }
+    }
+)
 
 CONFIG_SCHEMA = cv.All(
     cv.COMPONENT_SCHEMA.extend(
@@ -62,7 +80,7 @@ CONFIG_SCHEMA = cv.All(
             cv.Required(CONF_TIMER_SELECT): select.select_schema(TimerSelect),
             cv.Required(CONF_SAVE_BUTTON): button.button_schema(TimerSaveButton),
             cv.Required(CONF_DISABLE_SWITCH): switch.switch_schema(TimerDisableSwitch),
-            cv.Required(CONF_OUTPUTS): cv.ensure_list(cv.use_id(switch.Switch)),
+            cv.Required(CONF_OUTPUTS): cv.ensure_list(OUTPUT_SCHEMA),
             cv.Optional(CONF_NAMES): cv.ensure_list(cv.string),
         }
     ),
