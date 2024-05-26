cd6e022674a172bb73d3ac6457f87943dd7cb977
Samuel Sieb <samuel@sieb.net>
2024-05-25 23:46:01 -0700

    interface functionality

--- components/timer/timer.cpp
+++ components/timer/timer.cpp
@@ -33,63 +33,54 @@ or connect to: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
 ****************************************************/
 
 #include "timer.h"
-
+#include "esphome/core/helpers.h"
+#include "esphome/core/log.h"
 
 namespace esphome {
 namespace timer {
 
 static const char *const TAG = "timer";
 
-std::string Timer::to_string() const {
-    std::string result = "Live;" + std::to_string(live) +
-                            ",Mode;" + std::to_string(mode);
+std::string TimerData::to_string() const {
+    std::string result = str_sprintf("Live;%d,Mode;%d", this->live, this->mode);
 
     // Include time only if mode is 0
-    if (mode == 0) {
-        result += ",Time;" + std::to_string(hour) + ":" +
-                    (minute < 10 ? "0" + std::to_string(minute) : std::to_string(minute));
-    }
-
-    result += ",Repeat;" + std::to_string(repeat) +
-                ",Days;" +
-                (days.day.sun ? "S" : "-") +
-                (days.day.mon ? "M" : "-") +
-                (days.day.tue ? "T" : "-") +
-                (days.day.wed ? "W" : "-") +
-                (days.day.thu ? "T" : "-") +
-                (days.day.fri ? "F" : "-") +
-                (days.day.sat ? "S" : "-") +
-                ",Output;" + std::to_string(output) +
-                ",Action;" + std::to_string(action);
-
+    if (this->mode == 0) {
+        result += str_sprintf(",Time;%d:%02d", this->hour, this->minute);
+    } else if (this->hour != 0 || this->minute != 0) {
     // Include offset if it's not zero
-    if (offset_hour != 0 || offset_minute != 0) {
-        result += ",Offset;";
-        result += use_negative_offset ? '-' : '+';
-        result += std::to_string(offset_hour) + ":" +
-                    (offset_minute < 10 ? "0" + std::to_string(offset_minute) : std::to_string(offset_minute));
+        result += str_sprintf(",Offset;%c%d:%02d", use_negative_offset ? '-' : '+', this->hour, this->minute);
     }
+
+    result += str_sprintf(",Repeat;%d,Days;%c%c%c%c%c%c%c,Output;%d,Action;%d", this->repeat,
+                          this->days.day.sun ? 'S' : '-',
+                          this->days.day.mon ? 'M' : '-',
+                          this->days.day.tue ? 'T' : '-',
+                          this->days.day.wed ? 'W' : '-',
+                          this->days.day.thu ? 'T' : '-',
+                          this->days.day.fri ? 'F' : '-',
+                          this->days.day.sat ? 'S' : '-',
+                          this->output, this->action);
 
     return result;
 }
 
-void Timer::reset() {
-    live = false;
-    repeat = false;
-    use_negative_offset = false;
-    days.raw = 0;
-    mode = 0;
-    action = 0;
-    output = 0;
-    hour = 0;
-    minute = 0;
-    offset_hour = 0;
-    offset_minute = 0;
-    last_ran_timestamp = 0;
+void TimerData::reset() {
+    this->valid = false;
+    this->live = false;
+    this->repeat = false;
+    this->use_negative_offset = false;
+    this->days.raw = 0;
+    this->mode = 0;
+    this->action = 0;
+    this->output = 0;
+    this->hour = 0;
+    this->minute = 0;
+    this->last_ran_timestamp = 0;
 }
 
-void Timer::from_string(const std::string& settings) {
-    reset(); // resets the timer
+void TimerData::from_string(const std::string& settings) {
+    this->reset(); // resets the timer
     uint16_t index = 0;
     while (index < settings.size()) {
         // Find the next key-value pair
@@ -109,14 +100,14 @@ void Timer::from_string(const std::string& settings) {
 
         // Update struct settings based on the key-value pair
         if (key == "Live") {
-            live = std::stoi(value);
+            this->live = std::stoi(value);
         } else if (key == "Mode") {
-            mode = std::stoi(value);
+            this->mode = std::stoi(value);
         } else if (key == "Time") {
             size_t colonPos = value.find(':');
             if (colonPos != std::string::npos) {
-                hour = std::stoi(value.substr(0, colonPos));
-                minute = std::stoi(value.substr(colonPos + 1));
+                this->hour = std::stoi(value.substr(0, colonPos));
+                this->minute = std::stoi(value.substr(colonPos + 1));
             }
         } else if (key == "Repeat") {
             repeat = std::stoi(value);
@@ -124,21 +115,21 @@ void Timer::from_string(const std::string& settings) {
             for (size_t i = 0; i < value.size(); ++i) {
                 if (value[i] != '-' && value[i] != '0') {
                     switch (i) {
-                        case 0: days.day.sun = true; break;
-                        case 1: days.day.mon = true; break;
-                        case 2: days.day.tue = true; break;
-                        case 3: days.day.wed = true; break;
-                        case 4: days.day.thu = true; break;
-                        case 5: days.day.fri = true; break;
-                        case 6: days.day.sat = true; break;
+                        case 0: this->days.day.sun = true; break;
+                        case 1: this->days.day.mon = true; break;
+                        case 2: this->days.day.tue = true; break;
+                        case 3: this->days.day.wed = true; break;
+                        case 4: this->days.day.thu = true; break;
+                        case 5: this->days.day.fri = true; break;
+                        case 6: this->days.day.sat = true; break;
                         default: break;
                     }
                 }
             }
         } else if (key == "Output") {
-            output = std::stoi(value);
+            this->output = std::stoi(value);
         } else if (key == "Action") {
-            action = std::stoi(value);
+            this->action = std::stof(value);
         } else if (key == "Offset") {
             if (!value.empty()) {
                 size_t colonPos = value.find(':');
@@ -146,236 +137,112 @@ void Timer::from_string(const std::string& settings) {
                 if (value[0] == '-') {
                     use_negative_offset = true;
                     if (colonPos != std::string::npos) {
-                        offset_hour = std::stoi(value.substr(1, colonPos));
+                        this->hour = std::stoi(value.substr(1, colonPos));
                     }
                 } else if (value[0] == '+') {
                     use_negative_offset = false;
                     if (colonPos != std::string::npos) {
-                        offset_hour = std::stoi(value.substr(1, colonPos));
+                        this->hour = std::stoi(value.substr(1, colonPos));
                     }
                 } else {
                     use_negative_offset = false;
                     if (colonPos != std::string::npos) {
-                        offset_hour = std::stoi(value.substr(0, colonPos));
+                        this->hour = std::stoi(value.substr(0, colonPos));
                     }
                 }
-                offset_minute = std::stoi(value.substr(colonPos + 1));
+                this->minute = std::stoi(value.substr(colonPos + 1));
             }
         }
 
         // Move to the next key-value pair
         index = delimiterPos + 1;
     }
+    this->valid = true;
 }
 
-void doRelayAction(const uint8_t& i, const time_t& timestamp, const bool& set_relays) {
-    ESP_LOGD("doRelayAction", "------ doRelayAction ran ------");
-   
-    // when not setting relays we just run the other actions below
-    if (set_relays) {
-        uint8_t output = id(global_timers)[i].output;
-        uint8_t action = id(global_timers)[i].action;
-
-        switch (output) {
-            case 0:
-                switch (action) {
-                    case 2: 
-                        id(relay_0).toggle(); 
-                        break;
-                    default: 
-                        id(relay_0).publish_state(action); 
-                        break;
-                } break;
-            case 1:
-                switch (action) {
-                    case 2: 
-                        id(relay_1).toggle(); 
-                        break;
-                    default: 
-                        id(relay_1).publish_state(action); 
-                        break;
-                } break;
-            default: break;
-        }
-    }
-    id(global_timers)[i].last_ran_timestamp = timestamp;
-
-    // if repeat was disabled or not set deactivate timer after running
-    if(id(global_timers)[i].repeat == false) {
-        id(global_timers)[i].live = false;
-    }
+void Timer::setup() {
+  ESP_LOGD(TAG, "setting up");
+  this->choose_(0, true);
 }
 
-bool dayMatches(const esphome::ESPTime& date, const struct Timer& timer) {
-    // we get the value based on the position to check date 
-    if ((timer.days.raw & (1 << date.day_of_week - 1)) != 0) {
-        ESP_LOGD("dayMatches", "------ day matched ------");
-        return true;
-    }
-    return false;
+void Timer::loop() {
 }
 
-
-bool isSameDay(std::time_t timestamp1, std::time_t timestamp2) {
-    constexpr int SECONDS_PER_DAY = 24 * 60 * 60; // Number of seconds in a day
-
-    // Get the number of days since the epoch for each timestamp
-    time_t days1 = timestamp1 / SECONDS_PER_DAY;
-    time_t days2 = timestamp2 / SECONDS_PER_DAY;
-
-    // If the number of days is the same, then they are on the same day
-    return days1 == days2;
+void Timer::dump_config() {
 }
 
-void checkForMissedTimer(const uint8_t& i) {
-    // if didn't run in the past 24 hours and was supposed to.
-    // check if it was supposed to run in the current day sometime after 12am and before now.
-    // if it's today to todays check
-    // otherwise minus day by one and run check to see if it was supposed to run
+void Timer::set_num_timers(int count) {
+  for (int i = 0; i < count; i++)
+    this->timers_.push_back(TimerData());
+}
 
-    esphome::ESPTime date = id(sntp_time).now();
-    if(id(global_next_run)[i] == 0) {
-        // timer isn't set 
-        return;
-    }
-    if(id(global_next_run)[i] > date.timestamp) {
-        // isn't supposed to run yet 
-        return;
-    }
-    if(id(global_timers)[i].last_ran_timestamp > date.timestamp - 60 * 60 * 24) {
-        // timer ran within the past 24 hours
-        return;
-    }
-    if(isSameDay(id(global_next_run)[i], date.timestamp)) {
-        // timer was supposed to run today
-        doRelayAction(i, date.timestamp, true);
-    }
-} // checkForMissedTimer
+void Timer::set_timer_text(text::Text *txt) {
+  this->text_ = txt;
+  txt->add_on_state_callback([this](std::string state) {
+    if (!this->updating_)
+      this->set_timer_text(state);
+  });
+}
 
-// Function to process a single timer setting
-time_t getTimerTimestamp(const struct Timer& timer) {
-    // If the timer is disabled, skip checking the other values
-    if (timer.live == false) {
-        return 0;
-    }
-    // Get the current time
-    esphome::ESPTime date = id(sntp_time).now();
-    // If the day doesn't match return 0
-    if (dayMatches(date, timer) == false) {
-        return 0;
-    }
-    // run time match check
-    time_t timestamp = 0;
+void Timer::set_timer_select(select::Select *sel) {
+  this->select_ = sel;
+  sel->add_on_state_callback([this](std::string state, size_t index) {
+    this->choose_(index, false);
+  });
+}
 
+static void switch_output(switch_::Switch *sw, float action) {
+  if (action == 0)
+    sw->turn_off();
+  else if (action == 1)
+    sw->turn_on();
+  else if (action == 2)
+    sw->toggle();
+  else
+    ESP_LOGD(TAG, "invalid switch action %f", action);
+}
 
-    switch (timer.mode) {
-        case 1:
-            ESP_LOGD("getTimerTimestamp", "------ mode is set to sunrise ------");
-            date.hour = date.minute = date.second = 0;                          // uptime time to 00:00:00
-            date.recalc_timestamp_utc();
-            timestamp = id(mysun).sunrise(date, -0.833)->timestamp;
-            break;
-        case 2:
-            ESP_LOGD("getTimerTimestamp", "------ mode is set to sunset ------");
-            date.hour = date.minute = date.second = 0;                          // uptime time to 00:00:00
-            date.recalc_timestamp_utc();
-            timestamp = id(mysun).sunset(date, -0.833)->timestamp;
-            break;
-        default: 
-            ESP_LOGD("getTimerTimestamp", "------ mode is set to time ------");
-            date.second = 0;
-            date.hour = timer.hour;
-            date.minute = timer.minute;
-            struct tm tm;
-            tm = date.to_c_tm();
-            timestamp = mktime(&tm);
-            break;
-    }
+void Timer::add_switch_output(switch_::Switch *sw) {
+  using namespace std::placeholders;
+  this->outputs_.push_back(std::move(std::bind(switch_output, sw, _1)));
+}
 
-    // calculate offset
-    int offset_seconds = 0; // max value is 24 hours in seconds
-    if(timer.use_negative_offset == true) {
-        offset_seconds -= timer.offset_hour * 60 * 60;
-        offset_seconds -= timer.offset_minute * 60;
-    } else {
-        offset_seconds += timer.offset_hour * 60 * 60;
-        offset_seconds += timer.offset_minute * 60;
-    }
-    timestamp += offset_seconds;
-    ESP_LOGD("getTimerTimestamp", "------ timestamp  %lu ------", timestamp);
-    return timestamp;
-} // getTimerTimestamp
+void Timer::add_automation_output(Trigger<float> *trigger) {
+  using namespace std::placeholders;
+  this->outputs_.push_back(std::move(std::bind(&Trigger<float>::trigger, trigger, _1)));
+}
 
+void Timer::choose(int index) {
+  this->choose_(index, true);
+}
 
-void setTimerTimestamp(const uint8_t& i) {
-    id(global_next_run)[i] = getTimerTimestamp(id(global_timers)[i]);
-} // setTimerTimestamp
+void Timer::set_timer_text(const std::string &value) {
+  this->timers_[this->selected_timer_].from_string(value);
+  this->updating_ = true;
+  if (this->text_ != nullptr)
+    this->text_->make_call().set_value(this->timers_[this->selected_timer_].to_string()).perform();
+  this->updating_ = false;
+}
 
-// Function to process a list of timer settings
-void setAllTimersTimestamp() {
-    ESP_LOGD("setAllTimersTimestamp", "------ updating all timer timestamps ------");
-    for (uint8_t i = 0; i < id(num_of_timers); ++i) {
-        setTimerTimestamp(i);
-        checkForMissedTimer(i);
-    }
+void Timer::choose_(int index, bool update) {
+  ESP_LOGD(TAG, "selecting timer %d", index);
+  if (index >= this->timers_.size())
+    return;
+  if (index == this->selected_timer_)
+    return;
+  this->selected_timer_ = index;
+  if (this->text_ != nullptr)
+    this->text_->make_call().set_value(this->timers_[index].to_string()).perform();
+  if (update && (this->select_ != nullptr))
+    this->select_->make_call().set_index(index).perform();
+}
 
-} // setAllTimersTimestamp
+void TimerSelect::control(const std::string &value) {
+  this->publish_state(value);
+}
 
-
-void onInterval() {
-    ESP_LOGD("onInterval", "------ Ran interval ------");
-    if(id(override_timer).state == true) {
-        ESP_LOGD("onInterval", "------ Disable All Timers is on, skipping all timer checks ------");
-        return;
-    }
-    // set variables
-    esphome::ESPTime date = id(sntp_time).now();
-    date.timestamp -= date.timestamp % 60;   // set seconds to 0
-    // loop all timer timestamps
-    for (uint8_t i = 0; i < id(num_of_timers); i++) {
-        if(id(global_next_run)[i] > 0) {   // check if timer is active
-            // ESP_LOGD("interval", "------ Timer %i Active ------", i + 1);
-            // ESP_LOGD("interval", "------ Current Time %llu Next Run Time %llu Last Run Time %llu ------", date.timestamp, id(global_next_run)[i], id(global_last_run)[i]);
-            if(id(global_next_run)[i] == date.timestamp) { // check if time matches
-                ESP_LOGD("interval", "------ Timer %i matches current time ------", i + 1);
-                doRelayAction(i, date.timestamp, true);
-            }
-        }
-    }
-
-
-} // onInterval
-
-void onSelect() {
-    // ESP_LOGD("onSelect", "------ Ran onSelect ------");
-    // ESP_LOGD("onSelect", "%s", x.c_str());
-
-    uint8_t index = static_cast<int>(*id(select_timer).active_index());
-    // ESP_LOGD("onSelect", "------ Timer %i Selected ------", index);
-    // ESP_LOGD("onSelect", "------ Timer %i last run at %llu next run at %llu ------", num_timer, id(global_last_run)[num_timer - 1], id(global_next_run)[num_timer - 1]);
-    if(index > 0 && index <= id(num_of_timers)) {
-        // set num_timer to be base on position starting from 0
-        index -= 1;
-        // set text string
-        // ESP_LOGD("onSelect", "------ string %s ------", id(timer_text).state.c_str());
-        id(timer_text).publish_state(id(global_timers)[index].to_string());
-        ESP_LOGD("onSelect", "------ next run %lu ------", id(global_next_run)[index]);
-    } else {
-        id(timer_text).publish_state("");
-    }
-} // onSelect
-
-void onPressSave() {
-    ESP_LOGD("onPressSave", "------ Save Button Pressed ------");
-
-    uint8_t index = static_cast<int>(*id(select_timer).active_index());
-    if(index > 0 && index <= id(num_of_timers)) {
-        // set num_timer to be base on position starting from 0
-        index -= 1;
-        id(global_timers)[index].from_string(id(timer_text).state);
-        setTimerTimestamp(index);
-    }
-} // onPressSave
-
+void TimerText::control(const std::string &value) {
+  this->publish_state(value);
+}
 }  // namespace timer
 }  // namespace esphome
